4f2baa97392ed70735e8429aeb7f336047cb325d
sammoralesag <sammoralesag@gmail.com>
2024-10-28 12:10:28 -0500

    Initial project

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -0,0 +1,48 @@
+package com.plcoding.cryptotracker.crypto.presentation.models
+
+import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.coin.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
+
+data class CoinUi(
+    val id: String,
+    val rank: Int,
+    val name: String,
+    val symbol: String,
+    val marketCapUsd: DisplayableNumber,
+    val priceUsd: DisplayableNumber,
+    val pricePercent24Hr: DisplayableNumber,
+    @DrawableRes val icon: Int,
+)
+
+data class DisplayableNumber(
+    val value: Double,
+    val formatted: String,
+)
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        rank = rank,
+        name = name,
+        symbol = symbol,
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        priceUsd = priceUsd.toDisplayableNumber(),
+        pricePercent24Hr = pricePercent24Hr.toDisplayableNumber(),
+        icon = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatted = NumberFormat.getCurrencyInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatted.format(this)
+    )
+}
+
