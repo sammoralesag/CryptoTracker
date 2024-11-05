124e4e38d7418a88ec2fc6f2e38dae8d4482623a
sammoralesag <sammoralesag@gmail.com>
2024-11-04 23:21:51 -0600

    handling network errors

--- app/src/main/java/com/plcoding/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/cryptotracker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.plcoding.cryptotracker
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,8 +11,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.plcoding.cryptotracker.core.presentation.util.ObserveAsEvents
+import com.plcoding.cryptotracker.core.presentation.util.toString
+import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListEvent
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListScreen
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
@@ -26,6 +31,18 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val viewModel = koinViewModel<CoinListViewModel>()
                     val state by viewModel.state.collectAsStateWithLifecycle()
+                    val context = LocalContext.current
+                    ObserveAsEvents(viewModel.events) { event ->
+                        when (event) {
+                            is CoinListEvent.Error -> {
+                                Toast.makeText(
+                                    context,
+                                    event.error.toString(context),
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                    }
                     CoinListScreen(
                         state = state,
                         modifier = Modifier.padding(innerPadding)
