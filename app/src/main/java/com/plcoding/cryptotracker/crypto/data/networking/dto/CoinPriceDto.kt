00e9cdc00bc5f5aed4f80fa42d3a00ddddcd4f46
sammoralesag <sammoralesag@gmail.com>
2024-11-17 23:07:10 -0600

    coin history call

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/dto/CoinPriceDto.kt
@@ -0,0 +1,10 @@
+package com.plcoding.cryptotracker.crypto.data.networking.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class CoinPriceDto(
+    val priceUsd: Double,
+    val time: Long
+)
+
--- app/src/main/java/com/plcoding/cryptotracker/crypto/domain/CoinDataSource.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/domain/CoinDataSource.kt
@@ -3,7 +3,13 @@ package com.plcoding.cryptotracker.crypto.domain
 import com.plcoding.cryptotracker.core.domain.util.NetworkError
 import com.plcoding.cryptotracker.core.domain.util.Result
 import com.plcoding.cryptotracker.crypto.domain.coin.Coin
+import java.time.ZonedDateTime
 
 interface CoinDataSource {
     suspend fun getCoins(): Result<List<Coin>, NetworkError>
+    suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError>
 }
