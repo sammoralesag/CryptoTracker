893b9b47089e0e3b01268b633e4b465b726e0520
sammoralesag <sammoralesag@gmail.com>
2024-11-18 19:00:31 -0600

    adding graph

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -3,6 +3,7 @@ package com.plcoding.cryptotracker.crypto.presentation.models
 import androidx.annotation.DrawableRes
 import com.plcoding.cryptotracker.core.presentation.util.getDrawableIdForCoin
 import com.plcoding.cryptotracker.crypto.domain.coin.Coin
+import com.plcoding.cryptotracker.crypto.presentation.coin_detail.DataPoint
 import java.text.NumberFormat
 import java.util.Locale
 
@@ -15,6 +16,7 @@ data class CoinUi(
     val priceUsd: DisplayableNumber,
     val changePercent24Hr: DisplayableNumber,
     @DrawableRes val iconRes: Int,
+    val coinPriceHistory: List<DataPoint> = emptyList()
 )
 
 data class DisplayableNumber(
